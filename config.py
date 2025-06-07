156bb8d3b9bf20c57d5fdc258d494c9b78711b65
gilangpambua <gilang.pambua@gmail.com>
2025-06-07 11:41:57 +0800

    fix9

--- config.py
+++ config.py
@@ -16,14 +16,8 @@ DETECTION_MODEL_DIR = ROOT / 'weights' / 'detection'
 YOLOv8n = DETECTION_MODEL_DIR / "Best_YOLOV8N.pt"
 YOLOv8s = DETECTION_MODEL_DIR / "Best_YOLOV8S.pt"
 YOLOv8m = DETECTION_MODEL_DIR / "Best_YOLOV8M.pt"
-YOLOv8nn = DETECTION_MODEL_DIR / "bestN.pt"
-YOLOv8sn = DETECTION_MODEL_DIR / "bestS.pt"
-YOLOv8mn = DETECTION_MODEL_DIR / "bestM.pt"
 
 DETECTION_MODEL_LIST = [
     "Best_YOLOV8N.pt",
     "Best_YOLOV8S.pt",
-    "Best_YOLOV8M.pt",
-    "bestN.pt",
-    "bestS.pt",
-    "bestM.pt"]
+    "Best_YOLOV8M.pt"]
